7979fb1f96c630212b66fb7d9cc01174f3cb236a
Ezequiel Thomas Arevalo <ezearevalodev@gmail.com>
2023-05-28 16:29:47 -0300

    v1.0.0

--- index.php
+++ index.php
@@ -1,16 +1,53 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
+    <!--Definición del documento-->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
 
-    <link rel="stylesheet" href="./res/css/styles.css">
+    <!--Información del sitio-->
+    <title>MyShop</title>
+    <meta name="description" content="MyShop es una tienda en línea que ofrece productos de calidad a precios asequibles. ¡Visítanos hoy mismo!">
+    <meta name="author" content="Ezequiel Thomas Arevalo">
+    <meta name="keywords" content="MyShop, tienda en línea, productos, calidad, precios asequibles">
+
+    <!--Favicon-->
+
+    <!--CSS-->
+    <link type="text/css" rel="stylesheet" href="./res/css/styles.css">
 </head>
 <body>
-    <?php 
-    echo "test";
-    ?>
+    <!--Inicio de Header-->
+    <header>
+        <div id="Header-Logo">
+            <h1>MyShop</h1>
+        </div>
+    </header>
+    <!--Fin de Header-->
+
+    <!--Inicio de Nav-->
+    <nav>
+        <ul>
+            <li><a href="index.php" class="list-item-link">Home</a></li>
+            <li><a href="index.php" class="list-item-link">Productos</a></li>
+            <li><a href="index.php" class="list-item-link">Contacto</a></li>
+        </ul>
+    </nav>
+    <!--Fin de Nav-->
+    
+    <!--Inicio del Main-->
+    <main>
+        <?php 
+            echo "vistas";
+        ?>
+    </main>
+    <!--Fin del Main-->
+
+    <!--Inicio del Footer-->
+    <footer id="Footer">
+        <p>Repositorio del proyecto <a href="https://github.com/Ezearevalodev/E-commerce" target="_blank">@Ezearevalodev</a></p>
+    </footer>
+    <!--Fin del Footer-->
 </body>
 </html>
