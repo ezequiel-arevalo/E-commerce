8c9374a769644e024a84b63c918a2137ab60f73f
Ezequiel Arevalo <ezequiel.arevalo@outlook.es>
2024-06-24 15:56:19 -0300

    fix: Correct improper Authentication class usage in realizar-compra.php
    - Fixed an issue in acciones/realizar-compra.php where the Authentication class was not utilized, leading to an error.
    - Adjusted the layout of buttons in "_carrito.php" for better alignment of "Empty Cart" and "Proceed to Checkout" actions.
    - Implemented validation to prevent actions like emptying or checking out from an empty cart.

--- sitio/acciones/realizar-compra.php
+++ sitio/acciones/realizar-compra.php
@@ -1,61 +1,78 @@
 <?php
 use App\Models\Carrito;
 use App\Models\Compras;
+use App\Auth\Autenticacion;
 
 session_start();
 require_once __DIR__ . '/../bootstrap/autoload.php';
 
-$usuarioId = $usuario->getUsuariosId();
-$carrito = new Carrito();
-$compras = new Compras();
+$autenticacion = new Autenticacion();
 
-if (isset($_POST['eliminar_producto'])) {
-    // Eliminar producto del carrito
-    $productoId = $_POST['producto_id'];
-    $carrito->eliminarProducto($usuarioId, $productoId);
-}
-
-if (isset($_POST['vaciar_carrito'])) {
-    // Vaciar carrito
-    $carrito->vaciarCarrito($usuarioId);
-}
-
-if (isset($_POST['restar_cantidad'])) {
-    // Restar cantidad de producto en el carrito
-    $productoId = $_POST['producto_id'];
-    $cantidadActual = $_POST['cantidad'];
-    $nuevaCantidad = max(1, $cantidadActual - 1);
-    $carrito->actualizarCantidad($usuarioId, $productoId, $nuevaCantidad);
-}
-
-if (isset($_POST['sumar_cantidad'])) {
-    // Sumar cantidad de producto en el carrito
-    $productoId = $_POST['producto_id'];
-    $cantidadActual = $_POST['cantidad'];
-    $nuevaCantidad = $cantidadActual + 1;
-    $carrito->actualizarCantidad($usuarioId, $productoId, $nuevaCantidad);
-}
-
-if (isset($_POST['realizar_compra'])) {
-    // Realizar compra
+if ($autenticacion->estaAutenticado()) {
+    $usuarioId = $autenticacion->getUsuarioId();
+    $carrito = new Carrito();
+    $compras = new Compras();
+    
+    // Verificar si el carrito tiene productos
     $productosEnCarrito = $carrito->obtenerProductos($usuarioId);
-    $detallesCompras = [];
-    foreach ($productosEnCarrito as $producto) {
-        $detallesCompras[] = [
-            'productoId' => $producto['productos_id'],
-            'cantidad' => $producto['cantidad']
-        ];
+    if (empty($productosEnCarrito)) {
+        $_SESSION['mensajeError'] = "El carrito debe tener productos.";
+        header("Location: ../index.php?s=_carrito");
+        exit;
     }
-
-    try {
-        $compras->realizarCompra($usuarioId, $detallesCompras);
+    
+    if (isset($_POST['eliminar_producto'])) {
+        // Eliminar producto del carrito
+        $productoId = $_POST['producto_id'];
+        $carrito->eliminarProducto($usuarioId, $productoId);
+    }
+    
+    if (isset($_POST['vaciar_carrito'])) {
+        // Vaciar carrito
         $carrito->vaciarCarrito($usuarioId);
-        $_SESSION['mensajeExito'] = "Compra realizada con éxito.";
-    } catch (Exception $e) {
-        $_SESSION['mensajeError'] = "Error al realizar la compra: " . $e->getMessage();
     }
+    
+    if (isset($_POST['restar_cantidad'])) {
+        // Restar cantidad de producto en el carrito
+        $productoId = $_POST['producto_id'];
+        $cantidadActual = $_POST['cantidad'];
+        $nuevaCantidad = max(1, $cantidadActual - 1);
+        $carrito->actualizarCantidad($usuarioId, $productoId, $nuevaCantidad);
+    }
+    
+    if (isset($_POST['sumar_cantidad'])) {
+        // Sumar cantidad de producto en el carrito
+        $productoId = $_POST['producto_id'];
+        $cantidadActual = $_POST['cantidad'];
+        $nuevaCantidad = $cantidadActual + 1;
+        $carrito->actualizarCantidad($usuarioId, $productoId, $nuevaCantidad);
+    }
+    
+    if (isset($_POST['realizar_compra'])) {
+        // Realizar compra
+        $productosEnCarrito = $carrito->obtenerProductos($usuarioId);
+        $detallesCompras = [];
+        foreach ($productosEnCarrito as $producto) {
+            $detallesCompras[] = [
+                'productoId' => $producto['productos_id'],
+                'cantidad' => $producto['cantidad']
+            ];
+        }
+    
+        try {
+            $compras->realizarCompra($usuarioId, $detallesCompras);
+            $carrito->vaciarCarrito($usuarioId);
+            $_SESSION['mensajeExito'] = "Compra realizada con éxito.";
+        } catch (Exception $e) {
+            $_SESSION['mensajeError'] = "Error al realizar la compra: " . $e->getMessage();
+        }
+    }
+    
+    // Redirigir de vuelta al carrito después de procesar la solicitud
+    header("Location: ../index.php?s=_carrito");
+    exit;
+} else {
+    // Redirigir al usuario a la página de inicio de sesión si no está autenticado
+    header("Location: ../index.php?s=_iniciar-sesion");
+    exit;
 }
-
-// Redirigir de vuelta al carrito después de procesar la solicitud
-header("Location: ../index.php?s=_carrito");
-exit;
--- sitio/vistas/_carrito.php
+++ sitio/vistas/_carrito.php
@@ -38,7 +38,7 @@ $total = $carrito->obtenerTotal($usuarioId);
                     <td class="text-center">
                         <div class="input-group flex justify-content-center">
                             <div class="input-group-prepend">
-                                <form action="acciones/realizar-compra.php" method="POST" style="display:inline;">
+                                <form action="acciones/realizar-compra.php" method="POST">
                                     <input type="hidden" name="producto_id" value="<?= $producto['productos_id']; ?>">
                                     <input type="hidden" name="cantidad" value="<?= $producto['cantidad']; ?>">
                                     <button class="btn btn-danger btn-sm btn-square" type="submit" name="restar_cantidad">-</button>
@@ -46,7 +46,7 @@ $total = $carrito->obtenerTotal($usuarioId);
                             </div>
                             <input type="number" name="count" id="count" class="input-square text-center" value="<?= $producto['cantidad']; ?>" readonly>
                             <div class="input-group-append">
-                                <form action="acciones/realizar-compra.php" method="POST" style="display:inline;">
+                                <form action="acciones/realizar-compra.php" method="POST">
                                     <input type="hidden" name="producto_id" value="<?= $producto['productos_id']; ?>">
                                     <input type="hidden" name="cantidad" value="<?= $producto['cantidad']; ?>">
                                     <button class="btn btn-success btn-sm btn-square" type="submit" name="sumar_cantidad">+</button>
@@ -57,7 +57,7 @@ $total = $carrito->obtenerTotal($usuarioId);
                     <td class="fix-col"><?= $producto['productos_price']; ?> $</td>
                     <td class="fix-col"><?= number_format($producto['productos_price'] * $producto['cantidad'], 2); ?> $</td>
                     <td class="fix-col">
-                        <form action="acciones/realizar-compra.php" method="POST" style="display:inline;">
+                        <form action="acciones/realizar-compra.php" method="POST">
                             <input type="hidden" name="producto_id" value="<?= $producto['productos_id']; ?>">
                             <button type="submit" name="eliminar_producto" class="btn btn-danger accion-btn w-100"><i class="bi bi-trash3"> </i>Borrar</button>
                         </form>
@@ -70,12 +70,14 @@ $total = $carrito->obtenerTotal($usuarioId);
     <div class="row justify-content-center m-auto mb-5 mt-3">
         <div class="col-auto w-50">
             <h4 class="text-center"><strong>Total: </strong><?= number_format($total, 2); ?> $</h4>
-            <form action="acciones/realizar-compra.php" method="POST" style="display:inline;">
-                <button type="submit" name="vaciar_carrito" class="btn btn-danger w-100 p-2">Vaciar Carrito</button>
-            </form>
-            <form action="acciones/realizar-compra.php" method="POST" style="display:inline;">
-                <button type="submit" name="realizar_compra" class="btn btn-success w-100 p-2">Pagar</button>
-            </form>
+            <div class="d-flex justify-content-between mb-3">
+                <form action="acciones/realizar-compra.php" method="POST" class="w-50 me-2">
+                    <button type="submit" name="vaciar_carrito" class="btn btn-danger w-100">Vaciar Carrito</button>
+                </form>
+                <form action="acciones/realizar-compra.php" method="POST" class="w-50">
+                    <button type="submit" name="realizar_compra" class="btn btn-success w-100">Pagar</button>
+                </form>
+            </div>
         </div>
     </div>
 </div>
